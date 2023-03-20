13e849fc59e7e08ab8b52a965d0b2c14d6a28115
elmaachy <elmaachy@gmail.com>
2023-03-20 16:29:36 -0700

    print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description :A C program print with puts
+ *
+ * Return : Always 0 sucsses
+ **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
