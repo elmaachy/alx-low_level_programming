b835ae216fc9d566daee886daae64a0ed45453df
elmaachy <elmaachy@gmail.com>
2023-03-21 15:41:30 -0700

    prin text with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main : Entry point
+ * main - Entry point
  *
- * Description  : A C program  use  printf
- *
- * Return : 0 (Success)
+ * Return : Always 0 (Success)
  **/
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
