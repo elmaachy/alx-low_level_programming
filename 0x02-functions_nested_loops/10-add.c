43fb5c4f8e04e1db9875143f6a608f0bd2c9849c
elmaachy <elmaachy@gmail.com>
2023-03-22 04:52:19 -0700

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ *  add  - Entry function
+ *
+ * Description:  function that adds two integer and return the result
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum of a & b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
