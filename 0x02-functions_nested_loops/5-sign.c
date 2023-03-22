0bd9bb1a39b25457b6387933ab9f9bcecbff28b3
elmaachy <elmaachy@gmail.com>
2023-03-22 01:14:38 -0700

    prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar(43);
 		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
-		printf("0");
+		_putchar(48);
 		return (0);
 	}
-	printf("-");
-	return (-1);
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
 }
