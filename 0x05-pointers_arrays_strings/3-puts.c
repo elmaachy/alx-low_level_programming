89e4422cf916e2942211d7ec389eb3068f8d450b
elmaachy <elmaachy@gmail.com>
2023-03-28 16:15:00 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string followed by new line
+ *
+ * @str : String to print
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	while (*str != "\0")
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar("\n");
+}
