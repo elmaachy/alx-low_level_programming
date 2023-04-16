2264170af7700d80658ae9e5b9f6796ebcd92fd2
elmaachy <elmaachy@gmail.com>
2023-04-15 19:12:06 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints programe name
+ * @argc: int
+ * @argv: list
+ * Retrun 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%\n", argv[0]);
+	return (0);
+}
